8b17b5ac2d3183acdf65a5fb7b7b81c6a6c16c57
Shay D. <dardenteysha@gmail.com>
2024-08-20 21:31:37 -0500

    Added GitHub repo URL.

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://x.com/Teysha_D" target="_blank">
+          Teysha Darden
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/teysha-d/react-weather-app" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
