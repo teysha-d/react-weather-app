0784d51a00cf0b1832e29c7ffc4bbe5a80d0b3d2
Shay D. <dardenteysha@gmail.com>
2024-08-20 22:49:00 -0500

    Files and codes added to the project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://x.com/Teysha_D" target="_blank" rel="noreferrer">
-          Teysha Darden
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/teysha-d/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://x.com/Teysha_D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Teysha Darden
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/teysha-d/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-shecodes-weather-app.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
