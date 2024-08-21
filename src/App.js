6fea40f64e6be5e39c4fc8f86205636e4c628b5c
Shay D. <dardenteysha@gmail.com>
2024-08-20 21:35:15 -0500

    Corrected links.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://x.com/Teysha_D" target="_blank">
+        <a href="https://x.com/Teysha_D" target="_blank" rel="noreferrer">
           Teysha Darden
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/teysha-d/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/teysha-d/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open sourced on GitHub
         </a>
       </footer>
